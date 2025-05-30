45d5243b8123b64e13cd36c8bb2780c0cb9e17a8
Shi_ni <devengrg6@gmail.com>
2025-05-30 17:27:02 +0545

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,33 +1,25 @@
-public class CurrentAccount extends BankAccount {
+import java.util.ArrayList;
+import java.util.List;
 
-    private static final double OVERDRAFT_LIMIT = 5000;
+public class Customer {
 
-    public CurrentAccount(
-            String name,
-            String accNumber,
-            double initialBalance
-    ) {
-        super(name, accNumber, initialBalance);
+    private String name;
+    private List<BankAccount> accounts;
+
+    public Customer(String name) {
+        this.name = name;
+        this.accounts = new ArrayList<>();
     }
 
-    @Override
-    public void deposit(double amount) {
-        balance += amount;
-        System.out.println("Deposited Rs. " + amount + " to Current Account.");
+    public void addAccount(BankAccount account) {
+        accounts.add(account);
     }
 
-    @Override
-    public void withdraw(double amount) throws InsufficientBalanceException {
-        if (amount > (balance + OVERDRAFT_LIMIT)) {
-            throw new InsufficientBalanceException(
-                    "Withdrawal failed: Overdraft limit exceeded!"
-            );
-        }
-        balance -= amount;
-        System.out.println(
-                "Withdrawn Rs. " + amount + " from Current Account."
-        );
+    public List<BankAccount> getAccounts() {
+        return accounts;
+    }
+
+    public String getName() {
+        return name;
     }
 }
-
-
